6f1d34df2cf52a69849729037412928a324bdeb5
Brendo Gomes Santana <brendogs2021@gmail.com>
2024-04-19 23:56:21 -0300

    parte do sobre mim feito com sucesso

--- src/Page/styled.js
+++ src/Page/styled.js
@@ -2,16 +2,13 @@ import styled from "styled-components";
 
 export const Banner = styled.section`
     padding: 20vh 1em 0 1em;
-
     display: flex;
-
     width: 100%;
-
-
-
+    
     align-items: center;
     justify-content: space-around;
 
+    min-height: 90vh;
 `;
 
 export const Escrita = styled.article`
@@ -46,4 +43,24 @@ export const ContainerImg = styled.figure`
     }
 `;
 
-export const Sobre = styled.section``;
+export const Sobre = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 1em;
+
+    h3{
+        font-size: 2em;
+        letter-spacing: 20px;
+        margin-bottom: 0.5em;
+
+    }
+
+    P{
+        color: #828282;
+        width: 100%;
+        max-width: 1080px;
+        text-align: center;
+        line-height: 2em;
+    }
+`;
